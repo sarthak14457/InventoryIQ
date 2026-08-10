22c0ebbc4bd7c0c583326618c895520dc42d20d5
sarthak14457 <poudelsarthak44@gmail.com>
2026-08-10 18:07:23 +0545

    Change API base URL to production endpoint

--- frontend/inventory.js
+++ frontend/inventory.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = "https://inventoryiq-1-w22g.onrender.com/api";
 
 // Token comes from the URL (set by login.html on redirect) and is kept in memory for this page only.
 const params = new URLSearchParams(window.location.search);
