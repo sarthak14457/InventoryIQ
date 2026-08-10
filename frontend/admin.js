40a31ad81533be05a54aac9752e78fe2ba20c28b
sarthak14457 <poudelsarthak44@gmail.com>
2026-08-10 17:48:32 +0545

    Change API base URL to local development server

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://inventoryiq-frontend.onrender.com";
+const API_BASE = "http://localhost:3001/api";
 
 const params = new URLSearchParams(window.location.search);
 const token = params.get("token");
